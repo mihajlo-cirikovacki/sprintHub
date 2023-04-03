a84a730401535a452667b2e0595472306535eed9
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2023-04-03 19:02:51 +0200

    Added board router.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
 import { teamRouter } from "~/server/api/routers/team";
+import { boardRouter } from "~/server/api/routers/board";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { teamRouter } from "~/server/api/routers/team";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   team: teamRouter,
+  board: boardRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/board.ts
@@ -0,0 +1,60 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const boardRouter = createTRPCRouter({
+  // QUERIES:
+  getAll: protectedProcedure
+    .input(z.object({ teamId: z.string() }))
+    .query(({ ctx }) => {
+      return ctx.prisma.board.findMany();
+    }),
+
+  getById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.board.findFirst({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  // MUTATIONS:
+  create: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.board.create({
+          data: {
+            name: input.name,
+            teamId: ctx.session.user.teamId,
+            columns: {
+              create: [
+                { name: "Backlog" },
+                { name: "Todo" },
+                { name: "In Progress" },
+                { name: "Done" },
+              ],
+            },
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.board.delete({
+          where: {
+            id: input.id,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }),
+});
