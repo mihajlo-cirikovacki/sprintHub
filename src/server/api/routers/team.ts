b7594fcad8a4067b80fab5e467c7157ec6f5cb9d
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2023-04-04 17:19:27 +0200

    Removed not necessary avatar zod validations for now.

--- src/server/api/routers/team.ts
+++ src/server/api/routers/team.ts
@@ -6,28 +6,10 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-const MAX_FILE_SIZE = 500000;
-const ACCEPTED_IMAGE_TYPES = [
-  "image/jpeg",
-  "image/jpg",
-  "image/png",
-  "image/webp",
-];
-
 export const CreateTeamSchema = z.object({
   name: z.string(),
   domain: z.string(),
-  avatar: z
-    .any()
-    .refine((files) => files?.length == 1, "Image is required.")
-    .refine(
-      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-      `Max file size is 5MB.`
-    )
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      ".jpg, .jpeg, .png and .webp files are accepted."
-    ),
+  avatar: z.string(),
 });
 
 export type CreateTeamType = z.infer<typeof CreateTeamSchema>;
