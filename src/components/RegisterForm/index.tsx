aaa5a85e7e36c5b5974bc518e57e67723fda08f8
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2023-04-05 15:52:06 +0200

    Refactored: Moved toogle in comp.

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -23,6 +23,18 @@ type RegisterValues = {
 
 const team = [{ name: 'test', id: '4234234f' }];
 
+const Toogle = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <label className="relative inline-flex cursor-pointer items-center">
+      <input type="checkbox" onClick={onClick} className="peer sr-only" />
+      <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+        Join Team
+      </span>
+    </label>
+  );
+};
+
 export const RegisterForm = () => {
   const [chooseTeam, setChooseTeam] = useState(false);
 
@@ -40,17 +52,7 @@ export const RegisterForm = () => {
     >
       {({ register, formState }) => (
         <>
-          <label className="relative inline-flex cursor-pointer items-center">
-            <input
-              type="checkbox"
-              onClick={() => setChooseTeam((toggle) => !toggle)}
-              className="peer sr-only"
-            />
-            <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Join Team
-            </span>
-          </label>
+          <Toogle onClick={() => setChooseTeam((toggle) => !toggle)} />
           {chooseTeam && team.length > 0 ? (
             <SelectField
               className="w-full p-1"
